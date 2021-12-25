d5431bf23e571e7d6431d7b7b295fbf2541a27b9
Krasav4ik01 <75470901+Krasav4ik01@users.noreply.github.com>
2021-12-25 16:49:06 +0600

    Add files via upload

---
+++ history of shop/script.js
@@ -0,0 +1,34 @@
+     function popup(){
+    var btn = document.getElementById("btn_white").innerHTML;
+    if(btn == "Log In"){
+      document.querySelector(".popup").style.display = "flex";
+      $(".popup").animate({opacity: '1'} , "slow");
+      document.body.style.overflow = "hidden";
+    }
+    else{
+      logout();
+    }
+  }
+
+  function close_popup(){
+    $(".popup").animate({opacity: '0'}, "slow");
+    document.body.style.margin = "0";
+    document.body.style.height = "100%";
+    // document.body.style.overflow = "scroll";
+    document.getElementById("username").value = "";
+    document.getElementById("pass").value = "";
+    document.querySelector(".popup").style.display = "none";
+  }
+
+function login(name){
+  document.querySelector(".user_profile").style.display = "inline";
+  document.getElementById("user_name").innerHTML = name;
+  document.getElementById("btn_white").innerHTML = "Log Out";
+  close_popup();
+}
+
+function logout(){
+  document.querySelector(".user_profile").style.display = "none";
+  document.getElementById("btn_white").innerHTML = "Log In";
+}
+
